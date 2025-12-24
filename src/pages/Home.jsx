243ad849c5c2270281a886727ad0edb2bff81afa
Daniel Ayodeji Obamiyi <obamiyiayodeji@gmail.com>
2025-12-24 18:51:33 +0100

    feat: set up routing, plugins, theme variables, and animations

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div min-h-screen >  
+    {/* Theme Toggle */}
+
+    {/* Backgroung Effects */}
+
+    {/* Navbar */}
+
+    {/* Main Content */}
+
+    {/* Footer */}
+    
+    </div>
+  )
+}
+
+export default Home
